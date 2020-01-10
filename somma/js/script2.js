ceb3dde495e8d506a3f7ac60c5019f151ea04aa1
manuelatrotta <manuelat88@hotmail.it>
2020-01-10 14:56:40 +0100

    sistemazione snack 3

--- somma/js/script2.js
+++ somma/js/script2.js
@@ -1,6 +1,6 @@
 alert('welcome');
-//Il software deve chiedere per 5 volte all’utente di inserire un
-//numero
+//Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for
+//step1(usando ciclo for): chiedere per 5 volte all’utente di inserire un numero
 var somma = 0;
 //for(i=0; i<5; i++){
 //  var numero = parseInt(prompt('inserire numero'));
@@ -8,7 +8,8 @@ var somma = 0;
 //  somma = somma + numero;
 //}
 //console.log(somma);
-//con while
+
+//step2:con while
 var i=0;
 while(i<5){
   var numero = parseInt(prompt('inserire numero'));
