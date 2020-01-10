eca6a1e007cfceb122f621f6e8d3bb5439bedef0
manuelatrotta <manuelat88@hotmail.it>
2020-01-10 15:46:57 +0100

    sistemazione snack 5 con obbligo inserimento numero. Risoluzione con ciclo for e while

--- dispari/js/script2.js
+++ dispari/js/script2.js
@@ -6,43 +6,31 @@ var arrayNumeri = [];
 console.log(arrayNumeri);
 //step2 chiedere 6 numeri all'utente e verificare se è dispare dividendolo per 2 e imponendo che il resto sia diverso da 0
 //soluzione con ciclo for
-//for(var i=0; i<6; i++;){
-//  var numeroUtente= parseInt(prompt(i + 'inserisci un numero'));
-//  console.log(isNan(numeroUtente));
+for(var i=0; i<6; i++){
+  var numeroUtente= parseInt(prompt(i + 'inserisci un numero'));
+  console.log(isNaN(numeroUtente));
 //richiesta numero se non è stato inserito
-//while(isNan(numeroUtente)){
-//numeroUtente =parseInt(prompt('Si prega di inserire un numero.Grazie'));
-//}
-//  if(numeroUtente % 2 !=0){
-//    console.log('dispari', numeroUtente);
-//    arrayNumeri.push(numeroUtente);
-//  }
-//}
-
-//console.log(arrayNumeri);
-
-
-//soluzione con ciclo while in cui il contatore si pone fuori e l'incremento nei comandi
-var i=0;
-while(i<6){
-var numeroUtente= parseInt(prompt('inserisci un numero'));
-console.log(numeroUtente);
-if(numeroUtente % 2 !=0){
-console.log('dispari', numeroUtente);
- arrayNumeri.push(numeroUtente);
-i++;
+  while(isNaN(numeroUtente)){
+  numeroUtente =parseInt(prompt('Si prega di inserire un numero.Grazie'));
+  }
+  if(numeroUtente % 2 !=0){
+  console.log('dispari', numeroUtente);
+  arrayNumeri.push(numeroUtente);
   }
 }
+
 console.log(arrayNumeri);
 
 
-
-
-//creare un array vuoto
-//var numeri = [];
-//for(var i=0; i<numeri.length; i++){
-//  var numero=numeri[i];
-//  if(numero%2 !=0){
-//  console.log(numero);
+//soluzione con ciclo while in cui il contatore si pone fuori e l'incremento nei comandi
+//var i=0;
+//while(i<6){
+//var numeroUtente= parseInt(prompt('inserisci un numero'));
+//console.log(numeroUtente);
+//if(numeroUtente % 2 !=0){
+//console.log('dispari', numeroUtente);
+// arrayNumeri.push(numeroUtente);
+//i++;
 //  }
 //}
+//console.log(arrayNumeri);
