23473f4d090d761d24131b3b20cf7b8219ef98b5
manuelatrotta <manuelat88@hotmail.it>
2020-01-10 15:53:30 +0100

    snack 6 risoluzione con for e while

--- cubo/js/script2.js
+++ cubo/js/script2.js
@@ -2,7 +2,16 @@
 alert('welcome');
 
 //Stampa il cubo dei primi N numeri, dove N è un numero
-var n = prompt('inserisci un numero');
-for(var i=1; i<=n; i++){
+//creare una variabile in cui il numero nella condizione viene data dall'utente.Stampare il cubo dei numeri antecedenti a quello dato dall'utente mediante formula Math.pow(base, esponente)
+//var n = prompt('inserisci un numero');
+//for(var i=1; i<=n; i++){
+//  console.log(Math.pow(i, 3));
+//}
+
+//risoluzione con while
+var n= prompt('inserisci un numero');
+i=1;
+while(i<=n){
   console.log(Math.pow(i, 3));
+  i++;
 }
