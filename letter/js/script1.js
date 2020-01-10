8e2fdab6f18ff6116c9eeebd7caae7904d5f8c6d
manuelatrotta <manuelat88@hotmail.it>
2020-01-10 14:52:56 +0100

    sistemazione snack 2

--- letter/js/script1.js
+++ letter/js/script1.js
@@ -1,14 +1,17 @@
 alert('welcome');
-//L’utente inserisce due parole in successione
+//L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+//step1: L’utente inserisce due parole in successione
 var parola1 = prompt('inserisci una parola');
 var parola2 = prompt('inserisci una seconda parola');
-//stampa prima la parola più corta
+
+//step2:stampa prima la parola più corta
 if(parola1.length == parola2.length){
   alert('hai inserito due parole della stessa lunghezza');
 }else if (parola1.length > parola2.length) {
   console.log(parola2);
   console.log(parola1);
 }else{
-  console.log(parola2);
   console.log(parola1);
+  console.log(parola2);
 }
--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,10 +1,11 @@
-alert('hola');
-//inserimento due numeri da parte dell'utente
+alert('enjoy!');
+//L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+//step 1: inserimento due numeri da parte dell'utente
 var numero1 = parseInt(prompt('inserisci un numero'));
 var numero2 = parseInt(prompt('inserisci un secondo numero'));
 console.log(numero1);
 console.log(numero2);
-//caso di numero uguale e condizioni per numero maggiore
+//step2: caso di numero uguale e condizioni per numero maggiore
 if(numero1 == numero2){
   alert('hai inserito due numeri uguali');
 }
